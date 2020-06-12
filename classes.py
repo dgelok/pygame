dde238e1fcdebe047e02d008b84fc119f84b010f
Dan Gelok <dgelok@gmail.com>
2020-06-12 13:27:19 -0500

    monster moving in 8 directions plus a pause

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 import random
 
+speed = 2
 class Monster():
     def __init__(self, x, y, direction, counter, width, height):
         self.x = x
@@ -12,18 +13,30 @@ class Monster():
     def move(self):
         self.counter += 1
         if self.counter == 20:
-            self.direction = random.randint(1,5)
+            self.direction = random.randint(1,9)
             self.counter = 0
 
         if self.direction == 1:
-            self.x += 2
+            self.x += speed
         elif self.direction == 2:
-            self.x += -2
+            self.x += -speed
         elif self.direction == 3:
-            self.y += 2
+            self.y += speed
         elif self.direction == 4:
-            self.y += -2
-        
+            self.y += -speed
+        elif self.direction == 5:
+            self.x -= speed
+            self.y -= speed
+        elif self.direction == 6:
+            self.x += speed
+            self.y -= speed
+        elif self.direction == 7:
+            self.x += speed
+            self.y += speed
+        elif self.direction == 8:
+            self.x -= speed
+            self.y += speed
+
         if self.x >= self.width:
             self.x = 0
         elif self.x <= 0:
